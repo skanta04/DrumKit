d9ccfa584f53f93a9bbb4c3812b49d9d209ed901
saishreeya@gmail.com <skanta@ad.unc.edu>
2023-09-08 11:17:02 -0400

    adding sounds to each instrument

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
 for(var i = 0; i<document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+    document.querySelectorAll(".drum")[i].addEventListener("click", playSound);
 }
 
-function handleClick() {
-    alert("I got clicked");
+
+
+
+function playSound() {
+    
+    var buttonInnerHTML = this.innerHTML;
+
+    switch(buttonInnerHTML) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            var snare = new Audio("sounds/snare.png");
+            snare.play();
+        break;
+
+        case "k":
+            var crash = new Audio("sounds/crash.png");
+            crash.play();
+        break;
+
+        case "l":
+            var kick = new Audio("sounds/kick-bass.png");
+            kick.play();
+        break;
+
+        default:
+    }
 }
+
+
 
